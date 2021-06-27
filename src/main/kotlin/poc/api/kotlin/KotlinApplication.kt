69a27902256e30c9b94a83b00de426bd1b77a7b2
franciscosft <franciscosft@gmail.com>
2021-06-27 20:00:24 -0300

    Primeiro endpoint com Kotlin

--- src/main/kotlin/poc/api/kotlin/KotlinApplication.kt
+++ src/main/kotlin/poc/api/kotlin/KotlinApplication.kt
@@ -2,6 +2,8 @@ package poc.api.kotlin
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
 @SpringBootApplication
 class KotlinApplication
@@ -9,3 +11,16 @@ class KotlinApplication
 fun main(args: Array<String>) {
 	runApplication<KotlinApplication>(*args)
 }
+
+@RestController
+class MessageResource {
+	
+    @GetMapping
+    fun index(): List<Message> = listOf(
+        Message("1", "Hello!"),
+        Message("2", "Bonjour!"),
+        Message("3", "Privet!"),
+    )
+}
+
+data class Message(val id: String?, val text: String)
